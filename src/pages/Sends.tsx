96db99ba56c244d55e011fb03bf5f03ce5ce1ddd
heysamir <32209712-heysamir@users.noreply.replit.com>
2025-02-09 21:02:51 +0000

    Agent query: I've fixed the React component TypeScript errors. Can you access the staging environment now? Is the page loading?
    Update React import and add staging environment logs.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/1f44956e-bc47-48a8-a13e-c5f6222c2089/f778ab68-89bb-4d1f-bec3-dd46b471bb9a.jpg

--- src/pages/Sends.tsx
+++ src/pages/Sends.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import React, { FC, useState } from 'react'
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { useNavigate } from 'react-router-dom'
 import client from '../api/client'
