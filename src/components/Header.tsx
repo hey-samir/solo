89604f50eac9db552e0c15da3602e8e49acd3266
heysamir <32209712-heysamir@users.noreply.replit.com>
2025-02-10 03:13:36 +0000

    Agent query: Is the header now more compact and similar to the production version, with a smaller purple navbar and tiny Solo logo?
    Update staging environment to use production navbar styling.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/1f44956e-bc47-48a8-a13e-c5f6222c2089/ec808bba-96f1-4e85-9255-4d36bbbde291.jpg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import soloLogo from '../assets/solo.png'
 
 const Header: React.FC = () => {
   return (
-    <header className="navbar navbar-dark py-2" style={{ backgroundColor: 'var(--solo-purple)' }}>
-      <div className="container-fluid px-3">
+    <header className="navbar navbar-dark py-1" style={{ backgroundColor: 'var(--solo-purple)' }}>
+      <div className="container-fluid px-2">
         <Link to="/" className="navbar-brand m-0 p-0">
           <img 
             src={soloLogo}
             alt="Solo Logo" 
-            height="24"
+            height="20"
             className="d-inline-block"
             style={{ objectFit: 'contain' }}
           />
